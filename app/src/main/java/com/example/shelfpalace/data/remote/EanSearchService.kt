bdce499f325cdaec372fa6b1376d7a99d462d553
DoonPeroo <duplikatorr@gmail.com>
2026-09-22 01:54:56 +0200

    Update

---
+++ app/src/main/java/com/example/shelfpalace/data/remote/EanSearchService.kt
@@ -0,0 +1,3 @@
+package com.example.shelfpalace.data.remote
+
+// External Barcode APIs removed. Barcode scanning is handled 100% locally.
---
+++ app/src/main/java/com/example/shelfpalace/data/remote/IdealoService.kt
@@ -0,0 +1,3 @@
+package com.example.shelfpalace.data.remote
+
+// External Barcode APIs removed. Barcode scanning is now handled 100% locally.
